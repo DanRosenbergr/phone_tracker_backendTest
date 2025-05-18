ddf6e8b99a4a46bfd03d07088e9f128944fc73ef
DanRosenbergr <drosenbergr@gmail.com>
2025-05-18 17:14:26 +0200

    update fro URL connection format

--- db.js
+++ db.js
@@ -1,12 +1,19 @@
-const { Pool } = require("pg"); //Importuje třídu Pool z balíčku pg.
-require("dotenv").config(); //Načítá obsah souboru .env (environment variables) a zpřístupní proměnné v aplikaci pomocí process.env.
+const { Pool } = require("pg");
+require("dotenv").config();
+
+// const pool = new Pool({
+//   user: process.env.PG_USER,
+//   host: process.env.PG_HOST,
+//   database: process.env.PG_DATABASE,
+//   password: process.env.PG_PASSWORD,
+//   port: process.env.PG_PORT,
+// });
 
 const pool = new Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // často potřeba u cloudových DB jako Render
+  },
 });
 
-module.exports = pool; //Exportuje tento pool, aby mohl být použit v jiných částech aplikace.
+module.exports = pool;
